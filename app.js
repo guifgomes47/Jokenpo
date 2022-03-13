7ce016128f94a1d98cda4840205ecab45d5f8dae
guifgomes47 <guifgomes16@gmail.com>
2022-03-13 05:06:23 -0300

    change css, icons, and add more visibility results

--- app.js
+++ app.js
@@ -2,63 +2,83 @@ const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('.button-choice');
+let img = document.createElement('img');
+let img1 = document.createElement('img');
 let userChoice;
 let computerChoice;
 
-possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
-    userChoice = e.target.id;
-    userChoiceDisplay.innerText = userChoice;
-    generateComputerChoice();
-    getResult();
-}));
+possibleChoices.forEach((possibleChoices) =>
+    possibleChoices.addEventListener('click', (e) => {
+        userChoice = e.target.id;
+        if (userChoice == 'Rock') {
+            img.id = 'mini-icon';
+            img.src = './assets/rock.png';
+        }
+        if (userChoice == 'Paper') {
+            img.id = 'mini-icon';
+            img.src = './assets/paper.png';
+        }
+        if (userChoice == 'Scissors') {
+            img.id = 'mini-icon';
+            img.src = './assets/scissors.png';
+        }
+        userChoiceDisplay.appendChild(img);
+        generateComputerChoice();
+        getResult();
+    })
+);
 
-function generateComputerChoice(){
+function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
 
-    if (randomNumber === 0)
-    {
+    if (randomNumber === 0) {
         computerChoice = 'Rock';
+        img1.id = 'mini-icon';
+        img1.src = './assets/rock.png';
     }
-    if (randomNumber === 1)
-    {
+    if (randomNumber === 1) {
         computerChoice = 'Paper';
+        img1.id = 'mini-icon';
+        img1.src = './assets/paper.png';
     }
-    if (randomNumber === 2)
-    {
+    if (randomNumber === 2) {
         computerChoice = 'Scissors';
+        img1.id = 'mini-icon';
+        img1.src = './assets/scissors.png';
     }
 
-    computerChoiceDisplay.innerText = computerChoice;
+    computerChoiceDisplay.appendChild(img1);
 }
 
-function getResult(){
-    if (computerChoice === userChoice)
-    {
+function getResult() {
+    if (computerChoice === userChoice) {
         result = 'Draw';
+        resultDisplay.style.color = 'white';
     }
-    if (computerChoice === 'Rock' && userChoice === 'Paper')
-    {
+    if (computerChoice === 'Rock' && userChoice === 'Paper') {
         result = 'Win';
+        resultDisplay.style.color = 'green';
     }
-    if (computerChoice === 'Rock' && userChoice === 'Scissors')
-    {
-        result = 'Looser';
+    if (computerChoice === 'Rock' && userChoice === 'Scissors') {
+        result = 'Loser';
+        resultDisplay.style.color = '#C3073F';
     }
-    if (computerChoice === 'Paper' && userChoice === 'Rock')
-    {
-        result = 'Looser';
+    if (computerChoice === 'Paper' && userChoice === 'Rock') {
+        result = 'Loser';
+        resultDisplay.style.color = '#C3073F';
     }
-    if (computerChoice === 'Paper' && userChoice === 'Scissors')
-    {
+    if (computerChoice === 'Paper' && userChoice === 'Scissors') {
         result = 'Win';
+        resultDisplay.style.color = 'green';
     }
-    if (computerChoice === 'Scissors' && userChoice === 'Rock')
-    {
+    if (computerChoice === 'Scissors' && userChoice === 'Rock') {
         result = 'Win';
+        resultDisplay.style.color = 'green';
     }
-    if (computerChoice === 'Scissors' && userChoice === 'Paper')
-    {
-        result = 'Looser';
+    if (computerChoice === 'Scissors' && userChoice === 'Paper') {
+        result = 'Loser';
+        resultDisplay.style.color = '#C3073F';
     }
+
     resultDisplay.innerText = result;
 }
